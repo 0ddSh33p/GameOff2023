23b41b3a98d7a0addff033cf92f2d7c7e83f53b5
0ddSh33p <fastsmith3@gmail.com>
2023-11-02 21:51:53 -0400

    Working start
    YIPEE

--- Characters/RBFishMove.gd
+++ Characters/RBFishMove.gd
@@ -1,19 +1,30 @@
 extends RigidBody2D
 
-var speed = 3.0;
-var input;
-var inWater = false;
+var speed = 360
+var ramp = 25
+var input = Vector2(0,0)
+var inWater = false
+var size
+var grounded = false
 
-func _physics_process(delta):
-	var cur_speed = Vector2(0.0,0.0);
-	input = Vector2(
-		Input.get_action_raw_strength("ui_left") - Input.get_action_raw_strength("ui_right"),
-		Input.get_action_raw_strength("ui_up") - Input.get_action_raw_strength("ui_down")).normalized();
-	
-	#if(abs(linear_velocity.x) < speed):
-	cur_speed.x = speed * input.x;
+
+func _process(_delta):
+	size = abs(get_node("FishYGator").scale.x)
+	if(linear_velocity.x < -0.8):
+		get_node("FishYGator").scale.x = size
+	elif(linear_velocity.x > 0.8):
+		get_node("FishYGator").scale.x = size*-1
 		
-	#if(abs(linear_velocity.y) < speed && inWater):
-	cur_speed.y = speed * input.y;
-
-	apply_central_impulse(cur_speed);
+func _physics_process(_delta):
+	var space_state = get_world_2d().direct_space_state
+	input = Vector2(
+		Input.get_action_raw_strength("ui_right") - Input.get_action_raw_strength("ui_left"),
+		Input.get_action_raw_strength("ui_down") - Input.get_action_raw_strength("ui_up")).normalized()
+	
+	if(abs(linear_velocity.x) < speed):
+		linear_velocity.x += ramp * input.x
+	
+	if(inWater):
+		gravity_scale = 0.0;
+	else:
+		gravity_scale = 1.0;
